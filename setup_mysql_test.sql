cc46f3167cfd5aded7826222151c7b77e8e74220
mainanorbert <mainanorbert@gmail.com>
2023-11-20 13:25:14 +0000

    creating test mysql server

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Adding database (hbnb_test_db)
+-- Adding a new user hbnb_test (in localhost)
+-- The password of hbnb_test should be set to hbnb_test_pwd
+-- hbnb_test with all privileges on the database hbnb_test_db
+-- hbnb_test has a SELECT privilege on the database performance_schema
+
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
