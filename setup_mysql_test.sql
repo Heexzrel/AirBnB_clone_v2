3c27fea222caaea4bda28a4dfc891348ed6b848a
mainanorbert <mainanorbert@gmail.com>
2023-11-21 09:03:46 +0000

    adding test user to database

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,8 @@
--- Adding database (hbnb_test_db)
--- Adding a new user hbnb_test (in localhost)
+-- Adding database (hbnb_test_db) for project
+-- Adding a new user called hbnb_test (in localhost)
 -- The password of hbnb_test should be set to hbnb_test_pwd
 -- hbnb_test with all privileges on the database hbnb_test_db
--- hbnb_test has a SELECT privilege on the database performance_schema
-
+-- The user hbnb_test has a SELECT privilege on the database performance_schema
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
