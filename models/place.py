029a282caacf5759ba4bb51dc5120623320a996c
mainanorbert <mainanorbert@gmail.com>
2023-11-22 19:31:45 +0000

    importing storage clas

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
 
 
 assoc_table = Table("place_amenity", Base.metadata,
