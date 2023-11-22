4c0d5400361747aadf47e7fa724617a3e52601ce
mainanorbert <mainanorbert@gmail.com>
2023-11-22 14:30:55 +0000

    import storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, Column, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+import models
 
 Base = declarative_base()
 
--- models/place.py
+++ models/place.py
@@ -18,4 +18,6 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    # amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+    reviews = relationship("Review", backref="place", cascade="delete")
     amenity_ids = []
