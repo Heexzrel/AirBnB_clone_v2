cd0d0722bbf81a886c0f408d3cdc06fa4e99a33f
mainanorbert <mainanorbert@gmail.com>
2023-11-22 20:11:41 +0000

    place amenity

--- models/place.py
+++ models/place.py
@@ -5,13 +5,13 @@ from sqlalchemy import Column, Table, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
-place_amenity = Table("place_amenity", Base.metadata,
+"""place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
                              ForeignKey("places.id"), primary_key=True,
                              nullable=False),
                       Column("amenity_id", String(60),
                              ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False))
+                             primary_key=True, nullable=False))"""
 
 
 class Place(BaseModel, Base):
