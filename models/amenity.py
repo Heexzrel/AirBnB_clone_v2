5e634aa8da190f3f17ca45214431682d9868ee03
mainanorbert <mainanorbert@gmail.com>
2023-11-24 04:37:25 +0000

    my models

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """amenities class"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)
+    place_amenities =  Table("place_amenity", Base.metadata,
+            Column('place_id', String(60), ForeignKey('place.id'), primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
