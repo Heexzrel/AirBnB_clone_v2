ad7657c96067ab0b46e25cfb8e92e4e168739430
mainanorbert <mainanorbert@gmail.com>
2023-11-24 04:43:59 +0000

    many to many

--- models/amenity.py
+++ models/amenity.py
@@ -11,6 +11,8 @@ class Amenity(BaseModel, Base):
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)
-    place_amenities =  Table("place_amenity", Base.metadata,
-            Column('place_id', String(60), ForeignKey('place.id'), primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+    """place_amenities =  Table("place_amenity", Base.metadata,
+            Column('place_id', String(60), ForeignKey('place.id'),
+            primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'),
+            primary_key=True, nullable=False))"""
