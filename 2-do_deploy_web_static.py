7796b6694f44e880cb71d3a6ee7b48c4bae6c211
Norberts <mainanorbert@gmail.comm>
2023-12-07 19:33:14 +0300

    deploying to a remote server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from fabric.api import run, put, env, sudo, local
+from os.path import exists
+from datetime import datetime
+
+env.hosts = ['18.206.208.154', '100.26.236.42']
+
+def do_deploy(archive_path):
+    """function that distributes an archive to a web server.
+
+    Args:
+        archive_path (str): Path of the archive to distribute.
+    Returns:
+        False if the file doesn't exist at archive_path or an error occurs.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
