edd4883b26aafc37a2cf2e563ac997e1b1411265
Norberts <mainanorbert@gmail.comm>
2023-12-08 03:41:37 +0300

    config with puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,45 +1,91 @@
-# Configuring my Ubuntu for deployment
+# Configures a web server for deployment of web_static.
 
-exec { 'apt_update':
-  command => 'apt-get -y update',
-}
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
 package { 'nginx':
-  ensure => installed,
-}
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-file { ['/data/web_static/releases/test/', '/data/web_static/shared/']:
-  ensure => directory,
-}
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => 'Testing my server',
-}
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
 
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test/',
-  force  => true,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-file_line { 'nginx_location_block':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '        location /hbnb_static/ {',
-  after  => '        location / {',
-  before => '        location ~ \\.php$ {',
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('path/to/nginx_config.erb'),
-  notify  => Service['nginx'],
-}
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
