7de3fbc4e322fffaf0618a59c9c00ea2208a7dce
Norberts <mainanorbert@gmail.comm>
2023-12-08 03:36:10 +0300

    shellcheck for my project

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,12 +4,12 @@
 sudo apt-get -y update
 # sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "Testing my server" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-sudo sed -i '17i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '17i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
+exit 0
