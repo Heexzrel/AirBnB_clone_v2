7bb8cc0749cec7a1eccb771c3369c200aaeaefdd
Norberts <mainanorbert@gmail.comm>
2023-12-08 04:03:26 +0300

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,11 +9,11 @@ def do_pack():
     """Creating a tar gzipped archive of the directory web_static."""
     d_t = datetime.utcnow()
     file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(d_t.year,
-                                                            d_t.month,
-                                                            d_t.day,
-                                                            d_t.hour,
-                                                            d_t.minute,
-                                                            d_t.second)
+                                                              d_t.month,
+                                                              d_t.day,
+                                                              d_t.hour,
+                                                              d_t.minute,
+                                                              d_t.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
