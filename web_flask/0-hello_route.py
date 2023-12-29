d62ff0358044b302417f9310dbdc4c6ed1976a6a
Norberts <mainanorbert@gmail.comm>
2023-12-29 09:08:48 +0300

    starting flask listening 0.0.0.0:5000

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,17 @@
+#!/usr/bin/python3
+""" Starting a flask web alication
+listening on 0.0.0.0, port 5000
+Routes: "/" that displays hello world
+"""
 from flask import Flask
-
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """function returns hello world"""
     return 'Hello HBNB!'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
