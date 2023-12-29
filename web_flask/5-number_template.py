6dfc94231e664401c3d1e939f9545bb799647f48
Norberts <mainanorbert@gmail.comm>
2023-12-29 17:42:43 +0300

    html temlate

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -46,6 +46,7 @@ def my_number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def html_temp(n):
+    """using templates"""
     return render_template('5-number.html', number=n)
 
 
