a01bde8378e81d4c12a86fa0b7cc0cf4be75eeb6
Norberts <mainanorbert@gmail.comm>
2024-01-01 17:19:59 +0300

    updating database

--- models/state.py
+++ models/state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from  models.city import City
+from models.city import City
 from sqlalchemy.orm import relationship
 from sqlalchemy import String, Column
 from os import getenv
 import models
+
 
 class State(BaseModel, Base):
     """ State class """
@@ -13,7 +14,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") !='db':
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def cities(self):
             list_of_cities = []
