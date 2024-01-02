fc0060deef1dbb7e38ec9ce2c8a61da66531e74a
Norberts <mainanorbert@gmail.comm>
2024-01-02 05:27:50 +0300

    listing cities with states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts flask listening on 0.0.0.0 port 5000
+uses storage to fetch data"""
+from flask import Flask, render_template
+from models import storage
+# from models.state import State
+
+
+app = Flask(__name__)
+
+
+def cities_state():
+    """lists states with their cities"""
+    my_states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=my_states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exc):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
