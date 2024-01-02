dc7d30cf35711f0268eb45c3ad90a109e31f8022
Norberts <mainanorbert@gmail.comm>
2024-01-02 04:47:31 +0300

    listing states from db

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,7 +3,7 @@
 uses storage to fetch data"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+# from models.state import State
 
 
 app = Flask(__name__)
@@ -17,10 +17,10 @@ def teardown_appcontext(Exception):
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """displaying all states listed by name"""
-    states = storage.all(State).values()
-    sort_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sort_states)
+    states = storage.all("State")
+    # sort_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
