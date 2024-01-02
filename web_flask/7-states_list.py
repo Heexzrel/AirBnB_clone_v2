232939ce074df2aa40f5580378c020699e2e9b2d
Norberts <mainanorbert@gmail.comm>
2024-01-02 04:24:14 +0300

    listing states from database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""starts flask listening on 0.0.0.0 port 5000
+uses storage to fetch data"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(Exception):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """displaying all states listed by name"""
+    states = storage.all(State).values()
+    sort_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sort_states)
+
+
+if __name__ == '__name__':
+    app.run(host='0.0.0.0', port=5000)
